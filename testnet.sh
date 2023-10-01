a4969492521329cdbf016e69dc26eb2d26c15715
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2023-10-01 12:37:28 +1100

    Fix Typo

--- testnet.sh
+++ testnet.sh
@@ -91,7 +91,7 @@ else
 fi
 
 
-# Generate the genesis. This will generate  based
+# Generate the genesis. This will generate validators based
 # on https://github.com/ethereum/eth2.0-pm/blob/a085c9870f3956d6228ed2a40cd37f0c6580ecd7/interop/mocked_start/README.md
 $PRYSM_CTL_BINARY testnet generate-genesis \
 --fork=capella \
