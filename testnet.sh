9b7053f7aa5c4558566cfd00136e78f54d6dc937
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2023-12-06 11:14:28 +1100

    x

--- testnet.sh
+++ testnet.sh
@@ -19,7 +19,7 @@ fi
 NETWORK_DIR=./network
 
 # Change this number for your desired number of nodes
-NUM_NODES=2
+NUM_RPC_NODES=0
 
 # Port information. All ports will be incremented upon
 # with more validators to prevent port conflicts on a single machine
@@ -30,16 +30,6 @@ GETH_WS_PORT=8100
 GETH_AUTH_RPC_PORT=8200
 GETH_METRICS_PORT=8300
 GETH_NETWORK_PORT=8400
-
-PRYSM_BEACON_RPC_PORT=4000
-PRYSM_BEACON_GRPC_GATEWAY_PORT=4100
-PRYSM_BEACON_P2P_TCP_PORT=4200
-PRYSM_BEACON_P2P_UDP_PORT=4300
-PRYSM_BEACON_MONITORING_PORT=4400
-
-PRYSM_VALIDATOR_RPC_PORT=7000
-PRYSM_VALIDATOR_GRPC_GATEWAY_PORT=7100
-PRYSM_VALIDATOR_MONITORING_PORT=7200
 
 
 trap 'echo "Error on line $LINENO"; exit 1' ERR
@@ -56,17 +46,12 @@ trap 'cleanup' SIGINT
 rm -rf "$NETWORK_DIR" || echo "no network directory"
 mkdir -p $NETWORK_DIR
 pkill geth || echo "No existing geth processes"
-pkill beacon-chain || echo "No existing beacon-chain processes"
-pkill validator || echo "No existing validator processes"
 pkill bootnode || echo "No existing bootnode processes"
 
 # Set Paths for your binaries. Configure as you wish, particularly
 # if you're developing on a local fork of geth/prysm
-GETH_BINARY=./dependencies/go-ethereum/build/bin/geth
-GETH_BOOTNODE_BINARY=./dependencies/go-ethereum/build/bin/bootnode
-PRYSM_CTL_BINARY=./dependencies/prysm/out/prysmctl
-PRYSM_BEACON_BINARY=./dependencies/prysm/out/beacon-chain
-PRYSM_VALIDATOR_BINARY=./dependencies/prysm/out/validator
+GETH_BINARY=../../imtbl-go-ethereum/build/bin/geth
+GETH_BOOTNODE_BINARY=../../imtbl-go-ethereum/build/bin/bootnode
 
 # Create the bootnode for execution client peer discovery. 
 # Not a production grade bootnode. Does not do peer discovery for consensus client
@@ -91,27 +76,34 @@ else
 fi
 
 
-# Generate the genesis. This will generate validators based
-# on https://github.com/ethereum/eth2.0-pm/blob/a085c9870f3956d6228ed2a40cd37f0c6580ecd7/interop/mocked_start/README.md
-$PRYSM_CTL_BINARY testnet generate-genesis \
---fork=deneb \
---num-validators=$NUM_NODES \
---chain-config-file=./config.yml \
---geth-genesis-json-in=./genesis.json \
---output-ssz=$NETWORK_DIR/genesis.ssz \
---geth-genesis-json-out=$NETWORK_DIR/genesis.json
+# Create the Validator
+NODE_DIR=$NETWORK_DIR/validator
+mkdir -p $NODE_DIR/execution
+mkdir -p $NODE_DIR/consensus
+mkdir -p $NODE_DIR/logs
+# We use an empty password. Do not do this in production
+geth_pw_file="$NODE_DIR/geth_password.txt"
+echo "" > "$geth_pw_file"
+# Create the secret keys for this node and other account details
+# Run the geth command and capture its output
+output=$($GETH_BINARY account new --datadir "$NODE_DIR/execution" --password "$geth_pw_file")
 
+# Extract the public address using grep and awk
+public_address=$(echo "$output" | grep -o 'Public address of the key:   0x[a-zA-Z0-9]*' | awk '{print $6}')
 
-# The prysm bootstrap node is set after the first loop, as the first
-# node is the bootstrap node. This is used for consensus client discovery
-PRYSM_BOOTSTRAP_NODE=
+# Now you can use the public_address variable in your script
+echo "The public address for the validator is: $public_address"
 
-# Calculate how many nodes to wait for to be in sync with. Not a hard rule
-MIN_SYNC_PEERS=$((NUM_NODES/2))
-echo $MIN_SYNC_PEERS is minimum number of synced peers required
+# Format the address to be 64 characters long, padded with zeros
+formatted_address=$(printf "%-64s" "$public_address")
 
-# Create the validators in a loop
-for (( i=0; i<$NUM_NODES; i++ )); do
+# Update the extradata field in the genesis file (clique specific)
+jq --arg address "$formatted_address" '.extradata = $address' "./genesis.json" > tmp.json && mv tmp.json "./genesis.json"
+
+cp ./genesis.json $NETWORK_DIR/genesis.json
+
+# Create the RPC nodes
+for (( i=0; i<$NUM_RPC_NODES; i++ )); do
     NODE_DIR=$NETWORK_DIR/node-$i
     mkdir -p $NODE_DIR/execution
     mkdir -p $NODE_DIR/consensus
@@ -123,12 +115,18 @@ for (( i=0; i<$NUM_NODES; i++ )); do
 
     # Copy the same genesis and inital config the node's directories
     # All nodes must have the same genesis otherwise they will reject eachother
-    cp ./config.yml $NODE_DIR/consensus/config.yml
-    cp $NETWORK_DIR/genesis.ssz $NODE_DIR/consensus/genesis.ssz
     cp $NETWORK_DIR/genesis.json $NODE_DIR/execution/genesis.json
 
     # Create the secret keys for this node and other account details
-    $GETH_BINARY account new --datadir "$NODE_DIR/execution" --password "$geth_pw_file"
+    # Run the geth command and capture its output
+    output=$($GETH_BINARY account new --datadir "$NODE_DIR/execution" --password "$geth_pw_file")
+
+    # Extract the public address using grep and awk
+    public_address=$(echo "$output" | grep -o 'Public address of the key:   0x[a-zA-Z0-9]*' | awk '{print $6}')
+
+    # Now you can use the public_address variable in your script
+    echo "The public address is: $public_address"
+
 
     # Initialize geth for this node. Geth uses the genesis.json to write some initial state
     $GETH_BINARY init \
@@ -160,65 +158,9 @@ for (( i=0; i<$NUM_NODES; i++ )); do
       --identity=node-$i \
       --maxpendpeers=$NUM_NODES \
       --verbosity=3 \
+      --mine \
+      --miner.etherbase "$public_address" \
       --syncmode=full > "$NODE_DIR/logs/geth.log" 2>&1 &
-
-    sleep 5
-
-    # Start prysm consensus client for this node
-    $PRYSM_BEACON_BINARY \
-      --datadir=$NODE_DIR/consensus/beacondata \
-      --min-sync-peers=$MIN_SYNC_PEERS \
-      --genesis-state=$NODE_DIR/consensus/genesis.ssz \
-      --bootstrap-node=$PRYSM_BOOTSTRAP_NODE \
-      --interop-eth1data-votes \
-      --chain-config-file=$NODE_DIR/consensus/config.yml \
-      --contract-deployment-block=0 \
-      --chain-id=${CHAIN_ID:-32382} \
-      --rpc-host=127.0.0.1 \
-      --rpc-port=$((PRYSM_BEACON_RPC_PORT + i)) \
-      --grpc-gateway-host=127.0.0.1 \
-      --grpc-gateway-port=$((PRYSM_BEACON_GRPC_GATEWAY_PORT + i)) \
-      --execution-endpoint=http://localhost:$((GETH_AUTH_RPC_PORT + i)) \
-      --accept-terms-of-use \
-      --jwt-secret=$NODE_DIR/execution/jwtsecret \
-      --suggested-fee-recipient=0x123463a4b065722e99115d6c222f267d9cabb524 \
-      --minimum-peers-per-subnet=0 \
-      --p2p-tcp-port=$((PRYSM_BEACON_P2P_TCP_PORT + i)) \
-      --p2p-udp-port=$((PRYSM_BEACON_P2P_UDP_PORT + i)) \
-      --monitoring-port=$((PRYSM_BEACON_MONITORING_PORT + i)) \
-      --verbosity=info \
-      --slasher \
-      --enable-debug-rpc-endpoints > "$NODE_DIR/logs/beacon.log" 2>&1 &
-
-    # Start prysm validator for this node. Each validator node will
-    # manage 1 validator
-    $PRYSM_VALIDATOR_BINARY \
-      --beacon-rpc-provider=localhost:$((PRYSM_BEACON_RPC_PORT + i)) \
-      --datadir=$NODE_DIR/consensus/validatordata \
-      --accept-terms-of-use \
-      --interop-num-validators=1 \
-      --interop-start-index=$i \
-      --rpc-port=$((PRYSM_VALIDATOR_RPC_PORT + i)) \
-      --grpc-gateway-port=$((PRYSM_VALIDATOR_GRPC_GATEWAY_PORT + i)) \
-      --monitoring-port=$((PRYSM_VALIDATOR_MONITORING_PORT + i)) \
-      --graffiti="node-$i" \
-      --chain-config-file=$NODE_DIR/consensus/config.yml > "$NODE_DIR/logs/validator.log" 2>&1 &
-
-
-    # Check if the PRYSM_BOOTSTRAP_NODE variable is already set
-    if [[ -z "${PRYSM_BOOTSTRAP_NODE}" ]]; then
-        sleep 5 # sleep to let the prysm node set up
-        # If PRYSM_BOOTSTRAP_NODE is not set, execute the command and capture the result into the variable
-        # This allows subsequent nodes to discover the first node, treating it as the bootnode
-        PRYSM_BOOTSTRAP_NODE=$(curl -s localhost:4100/eth/v1/node/identity | jq -r '.data.enr')
-            # Check if the result starts with enr
-        if [[ $PRYSM_BOOTSTRAP_NODE == enr* ]]; then
-            echo "PRYSM_BOOTSTRAP_NODE is valid: $PRYSM_BOOTSTRAP_NODE"
-        else
-            echo "PRYSM_BOOTSTRAP_NODE does NOT start with enr"
-            exit 1
-        fi
-    fi
 done
 
 # You might want to change this if you want to tail logs for other nodes
